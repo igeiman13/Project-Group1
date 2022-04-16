532c254e74a9b6b03dff5d9cf0b2508aee8d10ec
tsindhuri <tsindhuri@myseneca.ca>
2022-04-16 07:18:09 +0000

    network-variables file created

--- staging/webserver/install_httpd.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-yum -y update
-yum -y install httpd
-echo '<h1><center>Group 1 ! Group members: Minnu Thomas, Praveen Suresh, Neethu Dominic. Tejaswi</center></h1>
- <img src="https://finalproject-websitefiles.s3.us-east-1.amazonaws.com/tulip.jpeg"> <img src="https://finalproject-websitefiles.s3.us-east-1.amazonaws.com/hibiscus.jpeg">'   >  /var/www/html/index.html
-sudo systemctl start httpd
-sudo systemctl enable httpd
