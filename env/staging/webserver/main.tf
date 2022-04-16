3dfcd7d8cc96a4a48e0c7bc63442f6d873744152
Praveen Suresh <103491779+psuresh1seneca@users.noreply.github.com>
2022-04-16 05:18:22 -0400

    Update main.tf

--- env/staging/webserver/main.tf
+++ env/staging/webserver/main.tf
@@ -131,17 +131,18 @@ resource "aws_autoscaling_group" "asg" {
   }
 }
 
-# ASG Policy to Scale Out
-resource "aws_autoscaling_policy" "asg_scaleOutPolicy-staging" {
-    name = "staging-asgPolicy-scaleOut"
+    
+ # ASG Policy to Scale Out
+resource "aws_autoscaling_policy" "asg_scaleOutPolicy-dev" {
+    name = "dev-asgPolicy-scaleOut"
     scaling_adjustment = 1
     adjustment_type = "ChangeInCapacity"
     cooldown = 120
     autoscaling_group_name = aws_autoscaling_group.asg.name
 }
 
-resource "aws_cloudwatch_metric_alarm" "metric_alarm_scaleOut_staging" {
-    alarm_name = "staging-alarm-ScaleOut"
+resource "aws_cloudwatch_metric_alarm" "metric_alarm_scaleOut_dev" {
+    alarm_name = "dev-alarm-ScaleOut"
     comparison_operator = "GreaterThanOrEqualToThreshold"
     evaluation_periods = "1"
     metric_name = "MemoryUtilization"
@@ -151,24 +152,24 @@ resource "aws_cloudwatch_metric_alarm" "metric_alarm_scaleOut_staging" {
     threshold = "10"
     alarm_description = "This metric monitors ec2 memory for high utilization and deploy more instance if required"
     alarm_actions = [
-        "${aws_autoscaling_policy.agents-scale-up.arn}"
+        "${aws_autoscaling_policy.asg_scaleOutPolicy-dev.arn}"
     ]
     dimensions = {
-        AutoScalingGroupName = "${aws_autoscaling_group.agents.name}"
+        AutoScalingGroupName = "${aws_autoscaling_group.asg.name}"
     }
 }
 
 # ASG Policy to Scale in
-resource "aws_autoscaling_policy" "asg_scaleInPolicy-staging" {
-    name = "staging-asgPolicy-scaleIn"
+resource "aws_autoscaling_policy" "asg_scaleInPolicy-dev" {
+    name = "dev-asgPolicy-scaleIn"
     scaling_adjustment = -1
     adjustment_type = "ChangeInCapacity"
     cooldown = 120
-    autoscaling_group_name = "${aws_autoscaling_group.agents.name}"
+    autoscaling_group_name = "${aws_autoscaling_group.asg.name}"
 }
 
-resource "aws_cloudwatch_metric_alarm" "metric_alarm_scaleIn_staging" {
-    alarm_name = "staging-alarm-ScaleIn"
+resource "aws_cloudwatch_metric_alarm" "metric_alarm_scaleIn_dev" {
+    alarm_name = "dev-alarm-ScaleIn"
     comparison_operator = "LessThanOrEqualToThreshold"
     evaluation_periods = "1"
     metric_name = "MemoryUtilization"
@@ -178,12 +179,14 @@ resource "aws_cloudwatch_metric_alarm" "metric_alarm_scaleIn_staging" {
     threshold = "5"
     alarm_description = "This metric monitors ec2 memory for low utilization on agent hosts"
     alarm_actions = [
-        "${aws_autoscaling_policy.agents-scale-down.arn}"
+        "${aws_autoscaling_policy.asg_scaleInPolicy-dev.arn}"
     ]
     dimensions = {
-        AutoScalingGroupName = "${aws_autoscaling_group.agents.name}"
+        AutoScalingGroupName = "${aws_autoscaling_group.asg.name}"
     }
 }
+
+
 
 # Bastion Host
 resource "aws_instance" "bastion_host" {
