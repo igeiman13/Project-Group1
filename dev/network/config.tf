3aad944e296ebc3af76d98f665179569815df6c1
Praveen Suresh <103491779+psuresh1seneca@users.noreply.github.com>
2022-04-16 03:40:03 -0400

    Delete dev directory

--- dev/network/config.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "dev-group1-acsproject"
-    key    = "dev-network/terraform.tfstate"
-    region = "us-east-1"
-  }
-}
--- dev/webserver/config.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "dev-group1-acsproject"
-    key    = "dev-webserver/terraform.tfstate"
-    region = "us-east-1"
-  }
-}
