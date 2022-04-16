6bcb2b36a125c096e9ffe2dd0f840ea02c712848
Praveen Suresh <103491779+psuresh1seneca@users.noreply.github.com>
2022-04-16 05:07:32 -0400

    Editing ASG_Policy-Prod

--- env/prod/webserver/main.tf
+++ env/prod/webserver/main.tf
@@ -131,59 +131,58 @@ resource "aws_autoscaling_group" "asg" {
   }
 }
 
-resource "aws_autoscaling_policy" "asg_policy_up" {
-  name = "${var.env}-asgPolicy-scaleOut"
-  scaling_adjustment = 1
-  adjustment_type = "ChangeInCapacity"
-  cooldown = 120
-  autoscaling_group_name = aws_autoscaling_group.asg.name
+# ASG Policy to Scale Out
+resource "aws_autoscaling_policy" "asg_scaleOutPolicy-prod" {
+    name = "prod-asgPolicy-scaleOut"
+    scaling_adjustment = 1
+    adjustment_type = "ChangeInCapacity"
+    cooldown = 120
+    autoscaling_group_name = aws_autoscaling_group.asg.name
 }
 
-
-
-
-resource "aws_cloudwatch_metric_alarm" "asg_cpu_alarm_up" {
-  alarm_name = "${var.env}-alarm-ScaleOut"
-  comparison_operator = "GreaterThanOrEqualToThreshold"
-  evaluation_periods = "1"
-  metric_name = "CPUUtilization"
-  namespace = "AWS/EC2"
-  period = "120"
-  statistic = "Average"
-  threshold = "10"
-
-  dimensions = {
-    AutoScalingGroupName = aws_autoscaling_group.asg.name
-  }
-
-  alarm_description = "This metric monitor EC2 instance CPU utilization"
-  alarm_actions = [ aws_autoscaling_policy.asg_policy_up.arn ]
+resource "aws_cloudwatch_metric_alarm" "metric_alarm_scaleOut_prod" {
+    alarm_name = "prod-alarm-ScaleOut"
+    comparison_operator = "GreaterThanOrEqualToThreshold"
+    evaluation_periods = "1"
+    metric_name = "MemoryUtilization"
+    namespace = "System/Linux"
+    period = "120"
+    statistic = "Average"
+    threshold = "10"
+    alarm_description = "This metric monitors ec2 memory for high utilization and deploy more instance if required"
+    alarm_actions = [
+        "${aws_autoscaling_policy.agents-scale-up.arn}"
+    ]
+    dimensions = {
+        AutoScalingGroupName = "${aws_autoscaling_group.agents.name}"
+    }
 }
 
-resource "aws_autoscaling_policy" "asg_policy_down" {
-  name = "${var.env}-asgPolicy-ScaleIn"
-  scaling_adjustment = -1
-  adjustment_type = "ChangeInCapacity"
-  cooldown = 120
-  autoscaling_group_name = aws_autoscaling_group.asg.name
+# ASG Policy to Scale in
+resource "aws_autoscaling_policy" "asg_scaleInPolicy-prod" {
+    name = "prod-asgPolicy-scaleIn"
+    scaling_adjustment = -1
+    adjustment_type = "ChangeInCapacity"
+    cooldown = 120
+    autoscaling_group_name = "${aws_autoscaling_group.agents.name}"
 }
 
-resource "aws_cloudwatch_metric_alarm" "asg_cpu_alarm_down" {
-  alarm_name = "${var.env}-alarm-ScaleIn"
-  comparison_operator = "LessThanOrEqualToThreshold"
-  evaluation_periods = "1"
-  metric_name = "CPUUtilization"
-  namespace = "AWS/EC2"
-  period = "120"
-  statistic = "Average"
-  threshold = "5"
-
-  dimensions = {
-    AutoScalingGroupName = aws_autoscaling_group.asg.name
-  }
-
-  alarm_description = "This metric monitor EC2 instance CPU utilization"
-  alarm_actions = [ aws_autoscaling_policy.asg_policy_down.arn ]
+resource "aws_cloudwatch_metric_alarm" "metric_alarm_scaleIn_prod" {
+    alarm_name = "prod-alarm-ScaleIn"
+    comparison_operator = "LessThanOrEqualToThreshold"
+    evaluation_periods = "1"
+    metric_name = "MemoryUtilization"
+    namespace = "System/Linux"
+    period = "120"
+    statistic = "Average"
+    threshold = "5"
+    alarm_description = "This metric monitors ec2 memory for low utilization on agent hosts"
+    alarm_actions = [
+        "${aws_autoscaling_policy.agents-scale-down.arn}"
+    ]
+    dimensions = {
+        AutoScalingGroupName = "${aws_autoscaling_group.agents.name}"
+    }
 }
 
 # Bastion Host
